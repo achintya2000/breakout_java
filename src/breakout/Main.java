26b5dab86f1bea6945c182ddff836c61b22ef23a
Achintya Kumar <ask60@duke.edu>
2020-01-11 17:07:59 -0500

    Can now create an empty gui pane with javafx

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,13 +1,25 @@
 package breakout;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
-    /**
-     * Start of the program.
-     */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 800);
+        primaryStage.setTitle("Achintya's Breakout Game");
+        primaryStage.show();
+    }
+
+    // Initialize javaFX gui
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        launch(args);
     }
 }
