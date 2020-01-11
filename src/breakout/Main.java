04a66dd22a446d425cce170f344b212103dee142
Achintya Kumar <ask60@duke.edu>
2020-01-11 17:16:45 -0500

    constant variable created for title. stage now displays colored scene of size 800x800

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -3,18 +3,20 @@ package breakout;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-/**
- * Feel free to completely change this code or delete it entirely. 
- */
 public class Main extends Application {
+
+    public static final String TITLE = "Achintya's Breakout Game";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
         Scene scene = new Scene(root, 800, 800);
-        primaryStage.setTitle("Achintya's Breakout Game");
+        scene.setFill(Color.AZURE);
+        primaryStage.setTitle(TITLE);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
