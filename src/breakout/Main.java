47880177fec5d20869e4e98e6f5d2a8c71322d06
Achintya Kumar <ask60@duke.edu>
2020-01-11 17:25:11 -0500

    created a function that can write display text on the scene. Used it to display hello world on the main stage

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,9 +1,13 @@
 package breakout;
 
 import javafx.application.Application;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,9 +19,22 @@ public class Main extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, 800, 800);
         scene.setFill(Color.AZURE);
+
+        ObservableList list = root.getChildren();
+        list.add(writeText("Hello World", 45,200,200));
+
         primaryStage.setTitle(TITLE);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public Text writeText(String message, int font, int x, int y) {
+        Text text = new Text();
+        text.setFont(new Font(font));
+        text.setX(x);
+        text.setY(y);
+        text.setText(message);
+        return text;
     }
 
     // Initialize javaFX gui
