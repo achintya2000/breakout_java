b0cd6b60a50bed7a519b9ed571e7398e7cfdd71c
Achintya Kumar <ask60@duke.edu>
2020-01-11 21:48:52 -0500

    added game ball and initial velocity so it moves

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -10,26 +10,36 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.Random;
+
 public class Main extends Application {
 
     public static final String TITLE = "Achintya's Breakout Game";
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
     public static final int FRAMES_PER_SECOND = 60;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 
+    static Random random = new Random();
+    public static int BALL_SPEED_X = 60 + random.nextInt(20);
+    public static int BALL_SPEED_Y = 60 + random.nextInt(20);
+
     private Brick simpleBrick = new Brick(500, 500, 100, 50, Color.GRAY, 1, "simple");
     private Brick multiBrick = new Brick(625,500, 100, 50, Color.BLUEVIOLET, 3, "multi");
     private Brick gamePaddle = new Brick(350, 700, 70, 20, Color.BLACK, 3, "player");
+    private Circle ball = new Circle(400,690,10);
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root, 800, 800);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
         scene.setFill(Color.AZURE);
 
         ObservableList list = root.getChildren();
@@ -37,6 +47,7 @@ public class Main extends Application {
         list.add(simpleBrick);
         list.add(multiBrick);
         list.add(gamePaddle);
+        list.add(ball);
 
         primaryStage.setTitle(TITLE);
         primaryStage.setScene(scene);
@@ -62,7 +73,8 @@ public class Main extends Application {
     }
 
     private void step(double elapsedTime) {
-        
+        ball.setCenterX(ball.getCenterX() + BALL_SPEED_X * elapsedTime);
+        ball.setCenterY(ball.getCenterY() + BALL_SPEED_Y * elapsedTime);
     }
 
     public Text writeText(String message, int font, int x, int y) {
