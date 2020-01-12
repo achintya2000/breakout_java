7b2e7fd094ebedd5733f7197dda0cf5a28382ba5
Achintya Kumar <ask60@duke.edu>
2020-01-12 18:40:03 -0500

    ball file pushed properly this time

--- src/breakout/Ball.java
+++ src/breakout/Ball.java
@@ -1,4 +1,13 @@
 package breakout;
 
-public class Ball {
+import javafx.scene.image.Image;
+
+public class Ball extends Sprite {
+
+    Ball(String name, int lives, Image image, int x, int y) {
+        super(name, lives, image);
+        setX(x);
+        setY(y);
+    }
+
 }
