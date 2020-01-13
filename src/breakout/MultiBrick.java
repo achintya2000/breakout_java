b0aa6e986b61951403a63488e0004fb9c84c60b3
Achintya Kumar <ask60@duke.edu>
2020-01-12 22:59:25 -0500

    actually push the MultiBrick file

---
+++ src/breakout/MultiBrick.java
@@ -0,0 +1,11 @@
+package breakout;
+
+import javafx.scene.image.Image;
+
+public class MultiBrick extends Sprite {
+
+    MultiBrick(String name, int lives, Image image) {
+        super(name, lives, image);
+    }
+
+}
